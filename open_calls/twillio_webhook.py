b049f1a979c80ff57b12645e55d1e048447742fb
Jamison Coombs <jamiedude224@gmail.com>
2023-02-21 15:33:08 -0800

    change file structure

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -1,7 +1,8 @@
 import yaml
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
-import os
+from os.path import exists
+from os import getcwd
 from tools.logging import logger
 from things.actors import actor
 import random
@@ -15,8 +16,6 @@ from tools.nlp import *
 
 yml_configs = {}
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 with open('config.yml', 'r') as yml_file:
     yml_configs = yaml.safe_load(yml_file)
 
@@ -25,7 +24,7 @@ def handle_request():
     act = None
 
     # Check if the actor exists
-    if os.path.exists( f"users/{request.form['From']}.pkl") :
+    if exists( f"users/{request.form['From']}.pkl") :
         with open(f"users/{request.form['From']}.pkl", 'rb') as p:
             act = pickle.load(p) 
     else:
